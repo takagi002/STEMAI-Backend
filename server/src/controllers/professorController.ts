0a8708d2045d8775bdf409642202c1a291e7f72e
Jacob Rose <rose030@gannon.edu>
2023-02-06 00:33:41 +0000

    Merged PR 26: Creating Controllers

---
+++ server/src/controllers/professorController.ts
@@ -0,0 +1,108 @@
+const professerService = require('../services/professerService');
+
+const getProfessersC = async(req, res) => {
+    try{
+        const professers = await professerService.getProfessers({});
+
+        res.header('Access-Control-Allow-Origin', '*');
+        res.status(200).json(professers);
+    } catch(error) {
+        res.status(404).json({message: error.message});
+    }
+}
+
+const getOneProfesserByProfessorIDC = async(req, res) => {
+    const professor_ID = req.params.professor_ID
+
+    try{
+        const professer = await professerService.getOneProfesserByProfessorID(professor_ID);
+
+        res.header('Access-Control-Allow-Origin', '*');
+        res.status(200).json(professer);
+    } catch(error) {
+        res.status(404).json({message: error.message});
+    }
+}
+
+const getOneProfesserByObjectIDC = async(req, res) => {
+    const _id = req.params._id
+
+    try{
+        const professer = await professerService.getOneProfesserByObjectID(_id);
+
+        res.header('Access-Control-Allow-Origin', '*');
+        res.status(200).json(professer);
+    } catch(error) {
+        res.status(404).json({message: error.message});
+    }
+}
+
+const createProfesserC = async(req, res) => {
+    try{
+        let newProfesser = await professerService.createProfesser(req.body);
+
+        res.header('Access-Control-Allow-Origin', '*');
+        res.status(201).json(newProfesser);
+    } catch(error) {
+        res.status(400).json({message: error.message});
+    }
+}
+
+const updateProfesserByObjectIDC = async(req, res) => {
+    const _id = req.params._id;
+    try{
+        await professerService.updateProfesserByObjectID(_id, req.body);
+
+        res.header('Access-Control-Allow-Origin', '*');
+        res.status(202).json(_id);
+    } catch(error) {
+        res.status(401).json({message: error.message});
+    }
+}
+
+const updateProfesserByProfessorIDC = async(req, res) => {
+    const professor_ID = req.params.professor_ID;
+    try{
+        await professerService.updateProfesserByProfessorID(professor_ID, req.body);
+
+        res.header('Access-Control-Allow-Origin', '*');
+        res.status(202).json(professor_ID);
+    } catch(error) {
+        res.status(401).json({message: error.message});
+    }
+}
+
+const deleteProfesserByObjectIDC = async(req, res) => {
+    const _id = req.params._id;
+    try{
+        await professerService.deleteProfesserByObjectID(_id, req.body);
+
+        res.header('Access-Control-Allow-Origin', '*');
+        res.status(203).json(_id);
+    } catch(error) {
+        res.status(402).json({message: error.message});
+    }
+}
+
+const deleteProfesserByProfessorIDC = async(req, res) => {
+    const professor_ID = req.params.professor_ID;
+    try{
+        await professerService.deleteProfesserByProfessorID(professor_ID, req.body);
+
+        res.header('Access-Control-Allow-Origin', '*');
+        res.status(203).json(professor_ID);
+    } catch(error) {
+        res.status(402).json({message: error.message});
+    }
+}
+
+module.exports.getProfessorsC = getProfessersC;
+module.exports.getOneProfessorByObjectIDC = getOneProfesserByObjectIDC
+module.exports.getOneProfessorByProfessorIDC = getOneProfesserByProfessorIDC;
+module.exports.createProfessorC = createProfesserC;
+module.exports.updateProfessorByObjectIDC = updateProfesserByObjectIDC
+module.exports.updateProfessorByProfessorIDC = updateProfesserByProfessorIDC
+module.exports.deleteProfessorByObjectIDC = deleteProfesserByObjectIDC;
+module.exports.deleteProfessorByProfessorIDC = deleteProfesserByProfessorIDC;
+
+
---
+++ server/src/controllers/studentController.ts
@@ -0,0 +1,108 @@
+const studentService = require('../services/studentService');
+
+const getStudentsC = async(req, res) => {
+    try{
+        const students = await studentService.getStudents({});
+
+        res.header('Access-Control-Allow-Origin', '*');
+        res.status(200).json(students);
+    } catch(error) {
+        res.status(404).json({message: error.message});
+    }
+}
+
+const getOneStudentByStudentIDC = async(req, res) => {
+    const student_ID = req.params.student_ID
+
+    try{
+        const student = await studentService.getOneStudentByStudentID(student_ID);
+
+        res.header('Access-Control-Allow-Origin', '*');
+        res.status(200).json(student);
+    } catch(error) {
+        res.status(404).json({message: error.message});
+    }
+}
+
+const getOneStudentByObjectIDC = async(req, res) => {
+    const _id = req.params._id
+
+    try{
+        const student = await studentService.getOneStudentByObjectID(_id);
+
+        res.header('Access-Control-Allow-Origin', '*');
+        res.status(200).json(student);
+    } catch(error) {
+        res.status(404).json({message: error.message});
+    }
+}
+
+const createStudentC = async(req, res) => {
+    try{
+        let newStudent = await studentService.createStudent(req.body);
+
+        res.header('Access-Control-Allow-Origin', '*');
+        res.status(201).json(newStudent);
+    } catch(error) {
+        res.status(400).json({message: error.message});
+    }
+}
+
+const updateStudentByObjectIDC = async(req, res) => {
+    const _id = req.params._id;
+    try{
+        await studentService.updateStudentByObjectID(_id, req.body);
+
+        res.header('Access-Control-Allow-Origin', '*');
+        res.status(202).json(_id);
+    } catch(error) {
+        res.status(401).json({message: error.message});
+    }
+}
+
+const updateStudentByStudentIDC = async(req, res) => {
+    const student_ID = req.params.student_ID;
+    try{
+        await studentService.updateStudentByStudentID(student_ID, req.body);
+
+        res.header('Access-Control-Allow-Origin', '*');
+        res.status(202).json(student_ID);
+    } catch(error) {
+        res.status(401).json({message: error.message});
+    }
+}
+
+const deleteStudentByObjectIDC = async(req, res) => {
+    const _id = req.params._id;
+    try{
+        await studentService.deleteStudentByObjectID(_id, req.body);
+
+        res.header('Access-Control-Allow-Origin', '*');
+        res.status(203).json(_id);
+    } catch(error) {
+        res.status(402).json({message: error.message});
+    }
+}
+
+const deleteStudentByStudentIDC = async(req, res) => {
+    const student_ID = req.params.student_ID;
+    try{
+        await studentService.deleteStudentByStudentID(student_ID, req.body);
+
+        res.header('Access-Control-Allow-Origin', '*');
+        res.status(203).json(student_ID);
+    } catch(error) {
+        res.status(402).json({message: error.message});
+    }
+}
+
+module.exports.getStudentsC = getStudentsC;
+module.exports.getOneStudentByObjectIDC = getOneStudentByObjectIDC;
+module.exports.getOneStudentByStudentIDC = getOneStudentByStudentIDC;
+module.exports.createStudentC = createStudentC;
+module.exports.updateStudentByObjectIDC = updateStudentByObjectIDC;
+module.exports.updateStudentByStudentIDC = updateStudentByStudentIDC;
+module.exports.deleteStudentByObjectIDC = deleteStudentByObjectIDC;
+module.exports.deleteStudentByStudentIDC = deleteStudentByStudentIDC;
+
+
